c47a554370679f7d43d94c63dd62feee41773ed1
saidali77 <mamedovsaidali7@gmail.com>
2020-07-17 16:39:09 +0600

    test10

---
+++ task1.py
@@ -0,0 +1,14 @@
+# raining = False
+# fire = False
+# teacher_looking = True
+# while not raining
+#     print ("Run")
+#     if fire == True:
+#         break
+#     elif teacher_looking = True:
+#         continue
+#     else:
+#         print ("Go home")
+print(list(range(9)))
+for num in range(9):
+    print (num)
